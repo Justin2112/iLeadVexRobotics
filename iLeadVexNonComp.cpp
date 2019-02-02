fc811dd0764fc07776e06c2e2652774b23af6f33
Justin2112 <justin133337@gmail.com>
2019-02-01 22:35:36 -0500

    Fixed tank controls and toggle on 1/23/2019. Added liftControls function and contents on 1/30/2019. Added comments for easier reading on 2/1/2019.

--- iLeadVexNonComp.cpp
+++ iLeadVexNonComp.cpp
@@ -1,42 +1,52 @@
 #include "robot-config.h"
+bool tankControl;//boolean that when true enables tank control and when false enables arcade control
 
-bool tankControl;
+void toggleControl() { //Function that toggles control between arcade and tank by the use of a boolean
 
-void toggleControl() //Toggles control between arcade and tank; controls are arcade by default
-{
-	if (Controller1.ButtonR2.pressing() == 1)
+	if (tankControl == false)
 	{
-		if (tankControl == false)
-		{
-			tankControl = true;
-		}
-		else if (tankControl == true)
-		{
-			tankControl = false;
-		}
+		tankControl = true;
+	}
+	else if (tankControl == true)
+	{
+		tankControl = false;
 	}
 }
 
-void robotControls() //Tank and arcade controls for the robot
+void liftControls() //controls for the lift on the robot
 {
-	if (tankControl == false) //Arcade Control (1 joystick)
+	if (Controller1.ButtonR1.pressing() == true) //Lift Up
 	{
-		LeftMotor.spin(directionType::fwd, (Controller1.Axis3.value() + Controller1.Axis4.value()) / 2, velocityUnits::pct);
-		RightMotor.spin(directionType::fwd, (Controller1.Axis3.value() - Controller1.Axis4.value()) / 2, velocityUnits::pct);
+		LiftMotor.spin(directionType::fwd, 75, velocityUnits::pct);
 	}
-	else if (tankControl == true) //Tank Control (2 joysticks)
+	else if (Controller1.ButtonL1.pressing() == true) //LiftMotor will go down when the L1 button is pressed on the controller
 	{
-		LeftMotor.spin(directionType::fwd, (Controller1.Axis3.value()), velocityUnits::pct);
-		RightMotor.spin(directionType::fwd, (Controller1.Axis2.value()), velocityUnits::pct);
+		LiftMotor.spin(directionType::rev, 75, velocityUnits::pct); //makes the lift go down 
+	}
+	else //if no control statement is true then stops the lift motor and leaves it in position
+	{
+		LiftMotor.stop();
 	}
 }
 
-int main() {
+void robotControls() { //Tank and arcade controls for the robot
+	liftControls(); //enables lift controls for the robot
 
-	tankControl = false;
+	if (tankControl == false) { //Arcade Control (1 joystick)
+		LeftMotor.spin(directionType::fwd, (Controller1.Axis4.value() + Controller1.Axis3.value()) / 2, velocityUnits::pct); //Averages left joystick's x and y axis for smoother control
+		RightMotor.spin(directionType::fwd, (Controller1.Axis4.value() - Controller1.Axis3.value()) / 2, velocityUnits::pct); //averages left joystick's x and y axis for smoother control
+	}
 
-	while (true) {
-		toggleControl();
-		robotControls();
+	else if (tankControl == true) { //Tank Control (2 joysticks)
+		LeftMotor.spin(directionType::rev, (Controller1.Axis3.value()), velocityUnits::pct); //LeftMotor goes forward whenever the left joystick is pressed forward
+		RightMotor.spin(directionType::fwd, (Controller1.Axis2.value()), velocityUnits::pct); //RightMotor goes forward whenever the right joystick is pressed forward
+	}
+}
+
+int main() { //main function
+	tankControl = false; //Tank Control default state
+	while (true) { //infinite loop
+		Controller1.ButtonX.pressed(toggleControl); //runs the toggle control function whenever button x is pressed on the controller
+		robotControls(); //function for the controls on the robot
 	}
 }
